022e97991eb6a1ef8fce22bcae40ed141dc33eb9
Midnight <rickakell1@gmail.com>
2024-11-09 20:52:49 -0600

    Update enemy to use character 2D

--- scripts/WalkingTarget.gd
+++ scripts/WalkingTarget.gd
@@ -1,7 +1,20 @@
-extends RigidBody2D
+extends CharacterBody2D
+var speed = 10000
 
-const SPEED = 100
+func move_up(delta):
+	velocity.y = -speed * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position.x += SPEED * delta
+func move_down(delta):
+	velocity.y = speed * delta
+
+func move_left(delta):
+	velocity.x = -speed * delta
+
+func move_right(delta):
+	velocity.x = speed * delta
+
+func _physics_process(delta):
+	if !is_on_floor():
+		move_down(delta)
+	move_right(delta)
+	move_and_slide()
