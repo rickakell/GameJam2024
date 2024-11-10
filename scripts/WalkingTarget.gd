4edad4a3201d7d391f476a3586ed112ce26b7070
Midnight <rickakell1@gmail.com>
2024-11-09 21:05:03 -0600

    Enemies: Add ray cast to stay on screen. Enemies no longer collide with other enemies

--- scripts/WalkingTarget.gd
+++ scripts/WalkingTarget.gd
@@ -1,20 +1,27 @@
 extends CharacterBody2D
+
 var speed = 10000
 
-func move_up(delta):
-	velocity.y = -speed * delta
+var direction = 1
 
 func move_down(delta):
 	velocity.y = speed * delta
 
-func move_left(delta):
-	velocity.x = -speed * delta
+func move(delta):
+	velocity.x = speed * delta * direction
 
-func move_right(delta):
-	velocity.x = speed * delta
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
 	if !is_on_floor():
 		move_down(delta)
-	move_right(delta)
+	move(delta)
 	move_and_slide()
