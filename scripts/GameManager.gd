3682113195c495f9d3852f47162e47077523dc4e
Midnight <rickakell1@gmail.com>
2024-11-10 00:05:09 -0600

    Clean up GameManager code

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,17 +1,19 @@
 extends Node
 
-@export var mob_scene: PackedScene
+func _ready():
+	SignalBus.connect("headshot", _headshot)
+
+func _headshot():
+	add_point()
 
 var score = 0
-
 @onready var score_label = $ScoreLabel
-
 func add_point():
 	score += 1
 	score_label.text = "Score: " + str(score)
 
 var walking_enemy_scene = preload("res://scenes/WalkingTarget.tscn")
-
+@export var mob_scene: PackedScene
 func _on_enemy_spawn_timer_timeout():
 	var enemy = walking_enemy_scene.instantiate()
 	var spawn_location = $EnemyPath/SpawnPath
@@ -19,10 +21,3 @@ func _on_enemy_spawn_timer_timeout():
 	enemy.position = spawn_location.position
 	enemy.speed = randf_range(8000, 12000)
 	add_child(enemy)
-	print("attempted to spawn enemy at position" + str(enemy.position))
-
-func _ready():
-	SignalBus.connect("headshot", _headshot)
-
-func _headshot():
-	add_point()
